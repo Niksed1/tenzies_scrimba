7250dd1829d39f6225de9aeceb4a8f34eef191e5
Niksed1 <image22061@gmail.com>
2025-01-29 02:26:59 -0800

    added holding down dice, and game instructions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import './App.css';
 import Die from './components/Die';
 import { useState } from 'react';
+import { nanoid } from "nanoid"
 function App() {
+/**
+     * Challenge: Update the `rollDice` function to not just roll
+     * all new dice, but instead to look through the existing dice
+     * to NOT roll any that are being `held`.
+     * 
+     * Hint: this will look relatively similiar to the `hold`
+     * function below. When we're "rolling" a die, we're really
+     * just updating the `value` property of the die object.
+     */ 
 
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    const dice = new Array(10).fill(0).map(() => Math.floor(Math.random() * 6));
-    console.log(dice)
-    return dice
+    return new Array(10)
+          .fill(0)
+          .map(() => ({
+          value: Math.ceil(Math.random() * 6), 
+          isHeld: false,
+          id: nanoid()
+      }));
   }
 
   function reroll(){
-    setDice(prev => generateAllNewDice());
+    setDice(oldDice => oldDice.map(die => 
+        die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
+    ));
+  }
+
+  function hold(id){
+    setDice(oldDice => oldDice.map(x => {
+      if(x.id === id){
+        return {...x, isHeld: !x.isHeld}
+      }
+      return x;
+    }));
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="container">
-        {dice.map(x => <Die value={x} />)}
+        {dice.map(x => <Die key={x.id} id = {x.id} value={x.value} isHeld={x.isHeld} click={hold}/>)}
       </div>
       <button className="roll" onClick={reroll}>
         Roll
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,15 @@
 import './Die.css';
+import {useState} from 'react';
 
 export default function Die (props) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
     return (
         <div>
-            <button className="die">{props.value}</button>
+            <button className="die" style={styles} onClick={() => props.click(props.id)}>{props.value}</button>
         </div>
     );
 }
